872fef9ffd89108cc5ef3b508ec8f7aca2c24b36
karlie moyo <karlieemoyo@gmail.com>
2023-01-31 22:15:10 +0200

    Allocating the bytes size

---
+++ Data-100_types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Description: Allocating the size of data types
+ * Return: 0 Always success
+*/
+int main(void)
+{
+printf(" char size: %lu bytes\n", sizeof(char));
+printf(" int size: %lu bytes\n", sizeof(int));
+printf(" float size: %lu bytes\n", sizeof(float));
+printf(" double size: %lu bytes\n", sizeof(double));
+printf(" short size: %lu bytes\n", sizeof(short));
+printf(" long size: %lu bytes\n", sizeof(long));
+printf(" long double size: %lu bytes\n", sizeof(long double));
+
+return (0);    
+}
+
--- data-types.c
+++ data-types.c
@@ -1,17 +1,21 @@
+//the statements will be executed first
 #include <stdio.h>
 /**
  * main - main entry
  * Description: Get the prompt from the user
  * Return: Always 0 success
  */
+// where our program start and end
 int main(void) 
 {
 int a, b;
+// print the output
 printf(" Enter first number: ");
+// prompt from the user
 scanf("%d", &a);
 printf(" Enter second number: ");
 scanf(" %d", &b);
 printf(" %d + %d = %d\n", a, b, a + b);
-
+// the end of the program execution
 return (0);    
 }
