a97efdb4a3e17738ffd3a3f78df809001c06aa52
karlie moyo <karlieemoyo@gmail.com>
2023-02-01 23:22:11 +0200

    Characters to numbers

--- Data-100_types.c
+++ Data-100_types.c
@@ -13,6 +13,10 @@ printf(" double size: %lu bytes\n", sizeof(double));
 printf(" short size: %lu bytes\n", sizeof(short));
 printf(" long size: %lu bytes\n", sizeof(long));
 printf(" long double size: %lu bytes\n", sizeof(long double));
+printf(" unsigned int size: %lu bytes", sizeof(unsigned int));
+printf(" signed int size: %lu bytes\n", sizeof(signed int));
+printf(" signed char size: %lu bytes\n", sizeof(signed char));
+printf(" unsigned char size: %lu bytes\n", sizeof(unsigned char));
 
 return (0);    
 }
---
+++ char-demonstration.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Description: converting the characters to numbers
+ * Return: Always 0 success
+*/
+int main(void)
+{
+char c;
+printf(" Enter a character: ");
+scanf("%c", &c);
+printf(" ASCII value of %c is %d\n", c,c);
+
+return (0);    
+}
