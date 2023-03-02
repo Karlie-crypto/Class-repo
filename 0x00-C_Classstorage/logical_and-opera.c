c875e8dbfcc1de13e6dcbbd53899df510ade2023
Karlie-crypto <karlieemoyo@.com>
2023-03-02 03:54:07 -0800

    This logical and statement is true

--- 0x00-C_Classstorage/logical_and-opera.c
+++ 0x00-C_Classstorage/logical_and-opera.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - main entry
- * Description: checking the output if both statements is true
- * REturn 0 Always success
+ * Description: To see if both statements are true
+ * Return: Always 0 sucess
  */
 int main(void)
 {
-  int a, b;
+  int a = 100, b = 400;
   if (a < b && a != b)
     {
       printf("It is true ");
@@ -17,5 +17,5 @@ int main(void)
     }
 
   return (0);
+}
 
-} 
