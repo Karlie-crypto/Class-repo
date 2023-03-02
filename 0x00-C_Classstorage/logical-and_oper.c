2cc99190aa3d876631391a7d578e2e6c841139f6
Karlie-crypto <karlieemoyo@.com>
2023-03-02 03:40:29 -0800

    Locical and

---
+++ 0x00-C_Classstorage/logical-and_oper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Description: checking the statement if it is false
+ * Return 0 Always success
+ */
+int main(void)
+{
+  int a, b;
+  if (a < b && a != b)
+    {
+      printf("It is true ");
+    }
+  else
+    {
+      printf("It is false ");
+    }
+
+  return (0);
+
+} 
