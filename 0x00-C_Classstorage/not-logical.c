a15cdc845aa941a9f3e404c15bceb41a3f768541
Karlie-crypto <karlieemoyo@.com>
2023-03-02 14:16:01 -0800

    Logical not operators demonstrations

---
+++ 0x00-C_Classstorage/not-logical.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Description: 
+ * Return: Always 0 success
+ */
+int main(void)
+{
+  int a, b;
+  a = 5;
+  b = 10;
+  if ((a < b) && (!(b > a)))
+    {
+      printf("It is true\n ");
+    }
+  else
+    {
+      printf("It is false\n ");
+    }
+    
+    return (0);
+}
