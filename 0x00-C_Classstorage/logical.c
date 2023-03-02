a3f8b5960dc24d084beb46625f86d294d89b284e
Karlie-crypto <karlieemoyo@.com>
2023-03-02 04:21:47 -0800

    Testing if the statement is true or false

---
+++ 0x00-C_Classstorage/logical.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main entry
+ * Description: Tell us the output if it's true or false
+ * Return: Always 0 success
+ */
+int main(void)
+{
+  int a, b;
+  if ( a == b && a != b)
+    {
+      printf("It is true\n ");
+    }
+  else
+    {
+      printf("It is false\n ");
+    }
+  
+  return (0);
+}
+
